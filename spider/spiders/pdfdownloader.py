4603b764f277763d42edc1fe5b3f174b9ea758fe
mauroceaz <mauro.cesar.10@hotmail.com>
2020-07-07 17:36:32 -0300

    novas revistas adicionads

--- spider/spiders/pdfdownloader.py
+++ spider/spiders/pdfdownloader.py
@@ -84,3 +84,111 @@ class opus2(scrapy.Spider):
         loader = ItemLoader(item=SpiderItem(), selector=dl)
         loader.add_value('file_urls', dl)
         yield loader.load_item()
+
+class permusi(scrapy.Spider):
+    name = 'permusi'
+    start_urls = ["https://periodicos.ufmg.br/index.php/permusi/"]
+
+    def parse(self, response):
+        for link in response.xpath('//a[contains(@class,"obj")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse2)
+
+    def parse2(self, response):
+        dl = response.xpath('//a[contains(@class,"download")]/@href').extract()
+        loader = ItemLoader(item=SpiderItem(), selector=dl)
+        loader.add_value('file_urls', dl)
+        yield loader.load_item()
+
+class abem(scrapy.Spider):
+    name = 'abem'
+    start_urls = ["http://www.abemeducacaomusical.com.br/revistas/revistaabem/index.php/revistaabem/issue/archive",
+                 "http://www.abemeducacaomusical.com.br/revistas/revistaabem/index.php/revistaabem/issue/archive?issuesPage=2#issues"]
+
+    def parse(self, response):
+        for link in response.xpath('//h4/a/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse2)
+
+    def parse2(self, response):
+        for link in response.xpath('//div[contains(@id,"iss")]/a/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse3)
+
+    def parse3(self, response):
+        for link in response.xpath('//a[contains(@class,"file")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse4)
+
+    def parse4(self, response):
+        dl = response.xpath('//div/a[contains(@class,"action")]/@href').extract()
+        loader = ItemLoader(item=SpiderItem(), selector=dl)
+        loader.add_value('file_urls', dl)
+        yield loader.load_item()
+
+class hodie(scrapy.Spider):
+    name = 'hodie'
+    start_urls = ["https://www.revistas.ufg.br/musica/issue/archive/1",
+                "https://www.revistas.ufg.br/musica/issue/archive/2",
+                "https://www.revistas.ufg.br/musica/issue/archive/3",
+                "https://www.revistas.ufg.br/musica/issue/archive/4",
+                "https://www.revistas.ufg.br/musica/issue/archive/5",
+                "https://www.revistas.ufg.br/musica/issue/archive/6",
+                "https://www.revistas.ufg.br/musica/issue/archive/7",
+                "https://www.revistas.ufg.br/musica/issue/archive/8"]
+
+    def parse(self, response):
+        for link in response.xpath('//a[contains(@class,"tit")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse2)
+
+    def parse2(self, response):
+        for link in response.xpath('//a[contains(@class,"obj")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse3)
+
+    def parse3(self, response):
+        dl = response.xpath('//a[contains(@class,"download")]/@href').extract()
+        loader = ItemLoader(item=SpiderItem(), selector=dl)
+        loader.add_value('file_urls', dl)
+        yield loader.load_item()
+
+class orfeu(scrapy.Spider):
+    name = 'orfeu'
+    start_urls = ["http://www.revistas.udesc.br/index.php/orfeu/issue/archive"]
+
+    def parse(self, response):
+        for link in response.xpath('//h4/a/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse2)
+
+    def parse2(self, response):
+        for link in response.xpath('//div[contains(@id,"iss")]/a/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse3)
+
+    def parse3(self, response):
+        for link in response.xpath('//a[contains(@class,"file")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse4)
+
+    def parse4(self, response):
+        for link in response.xpath('//div/a[contains(@class,"action")]/@href').extract():
+            loader = ItemLoader(item=SpiderItem(), selector=link)
+            urlabsoluta = response.urljoin(link)
+            loader.add_value('file_urls', urlabsoluta)
+            yield loader.load_item()
+
+class revistamusica(scrapy.Spider):
+    name = 'revistamusica'
+    start_urls = ["https://www.revistas.usp.br/revistamusica/issue/archive",
+                "https://www.revistas.usp.br/revistamusica/issue/archive/2"]
+
+    def parse(self, response):
+        for link in response.xpath('//a[contains(@class,"tit")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse2)
+
+    def parse2(self, response):
+        for link in response.xpath('//a[contains(@class,"cover")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse3)
+
+    def parse3(self, response):
+        for link in response.xpath('//a[contains(@class,"obj")]/@href').extract():
+            yield scrapy.Request(link,meta={'dont_redirect': True}, callback=self.parse4)
+
+    def parse4(self, response):
+        dl = response.xpath('//a[contains(@class,"download")]/@href').extract()
+        loader = ItemLoader(item=SpiderItem(), selector=dl)
+        loader.add_value('file_urls', dl)
+        yield loader.load_item()
